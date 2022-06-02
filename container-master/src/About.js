36b868b8da9de7c9b9d31cf39b7d992c60adef95
saiful <saiful.islam@ixorasolution.com>
2022-06-02 13:11:24 +0600

    about changed

--- container-master/src/About.js
+++ container-master/src/About.js
@@ -5,8 +5,7 @@ const About = () => (
   <main id="about">
     <h2>About this site</h2>
     <p>
-      This website was created by{' '}
-      <a href="https://twitter.com/thecamjackson">Cam Jackson</a> to demonstrate
+      This website was created by{' '} to demonstrate
       just one way that micro frontends can be implemented.
     </p>
     <p>
@@ -16,18 +15,18 @@ const About = () => (
       applications that can be delivered to production by multiple teams
       independently.
     </p>
-    <p>
+    {/* <p>
       To read more about the technique, including a full explanation of how the
       code for this demo works, check out the{' '}
       <a href="https://martinfowler.com/articles/micro-frontends.html">
         long-form article that Cam wrote for martinfowler.com
       </a>.
-    </p>
+    </p> */}
     <p>
       If you just want to read the source code for yourself, it's all available
       on Github at{' '}
-      <a href="https://github.com/micro-frontends-demo">
-        https://github.com/micro-frontends-demo
+      <a href="https://github.com/saifulislam2015/MicroFrontEnd-Pattern">
+      https://github.com/saifulislam2015/MicroFrontEnd-Pattern
       </a>.
     </p>
   </main>
