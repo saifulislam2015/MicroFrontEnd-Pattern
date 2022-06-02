c9708a4e0c99986fdf032900d920db17f190bb5e
saiful <saiful.islam@ixorasolution.com>
2022-06-02 13:09:29 +0600

    init commit

---
+++ browse-master/src/index.js
@@ -0,0 +1,18 @@
+import 'react-app-polyfill/ie11';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { unregister } from './registerServiceWorker';
+
+window.renderBrowse = (containerId, history) => {
+  console.log('In renderbrowse')
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId),
+  );
+  unregister();
+};
+
+window.unmountBrowse = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
